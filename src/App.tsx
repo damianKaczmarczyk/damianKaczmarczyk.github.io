0849f7175ed1f0f90102ca66c9862864af032510
Damian Kaczmarczyk <damiankaczmarczyk0@gmail.com>
2022-05-28 14:29:35 +0200

    Add html

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import sun from './res/sun.jpg'
 import milkyWay from './res/milky-way.jpg'
 import './App.css'
 
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.module.min.js'
+import * as THREE from 'three'
 
 function App(){
   const scene = new THREE.Scene();
@@ -32,25 +32,38 @@ function App(){
 
   function animate() {
     requestAnimationFrame(animate);
-    planet.rotateX(0.001);
+    planet.rotateX(0.002);
     planet.rotateY(0.001);
     planet.rotateZ(0.001);
 
     renderer.render(scene, camera);
   }
-
-for (let i = 0; i < 200; i++) {
-    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-    const star = new THREE.Mesh(geometry, material);
-
-    const [x, y, z] = Array(3)
-      .fill(undefined)
-      .map(() => THREE.MathUtils.randFloatSpread(100));
-
-    star.position.set(x, y, z);
-    scene.add(star);
+  let prevPos = document.body.getBoundingClientRect().top;
+  function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    let multiplier = (prevPos > t ? 1 : -1) * 0.0003;
+    planet.position.z -= Math.abs(prevPos - t) * multiplier;
+    
+    //camera.position.setX(t * -0.0001);
+    camera.rotateY(Math.abs(prevPos - t) * multiplier);
+    console.log(camera.rotation);
+    //camera.position.setZ(t * -0.00001);
+    prevPos = t;
   }
+  document.body.onscroll = moveCamera
+
+  for (let i = 0; i < 200; i++) {
+      const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+      const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+      const star = new THREE.Mesh(geometry, material);
+
+      const [x, y, z] = Array(3)
+        .fill(undefined)
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+
+      star.position.set(x, y, z);
+      scene.add(star);
+    }
 
   animate();
   //help();
